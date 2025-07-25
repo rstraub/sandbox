c35edcf1a5520f5d6cee9ffcfb1ca06289835aac
Roy Straub <roy.straub@ns.nl>
2025-07-25 12:17:52 +0200

    Yahtzee calculation

--- src/test/kotlin/com/rstraub/sandbox/yahtzee/YahtzeeTest.kt
+++ src/test/kotlin/com/rstraub/sandbox/yahtzee/YahtzeeTest.kt
@@ -1,17 +1,41 @@
 package com.rstraub.sandbox.yahtzee
 
+import com.rstraub.sandbox.yahtzee.ScoreType.CHANCE
+import com.rstraub.sandbox.yahtzee.ScoreType.YAHTZEE
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
 class YahtzeeTest {
   @Test
   fun `chance returns sum of all dices`() {
-    Yahtzee.score(1, 2, 3, 4, 5) shouldBe 15
+    Yahtzee.score(1, 2, 3, 4, 5, CHANCE) shouldBe 15
+  }
+
+  @Test
+  fun `yahtzee returns score of 50 given all the same numbers`() {
+    Yahtzee.score(1, 1, 1, 1, 1, YAHTZEE) shouldBe 50
+  }
+
+  @Test
+  fun `yahtzee returns score of 0 given the numbers are not the same`() {
+    Yahtzee.score(1, 1, 1, 1, 2, YAHTZEE) shouldBe 0
   }
 }
 
+enum class ScoreType {
+  CHANCE,
+  YAHTZEE
+}
+
 object Yahtzee {
-  fun score(dice1: Int, dice2: Int, dice3: Int, dice4: Int, dice5: Int): Int {
-    return dice1 + dice2 + dice3 + dice4 + dice5
+  fun score(dice1: Int, dice2: Int, dice3: Int, dice4: Int, dice5: Int, scoreType: ScoreType): Int {
+    val dices = listOf(dice1, dice2, dice3, dice4, dice5)
+
+    if (scoreType == CHANCE) return dices.sum()
+
+    val isYahtzee = dices.distinct().size == 1
+    if (isYahtzee) return 50
+
+    return 0
   }
 }
